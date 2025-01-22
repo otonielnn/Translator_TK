783f2ca3bd0ab3ccd820c901e834033fbb29fc91
otonielnn <otoniel_2002@live.com>
2025-01-22 19:58:02 -0300

    feat: Button and function to translate
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- translator.py
+++ translator.py
@@ -1,14 +1,18 @@
-from tkinter import Tk, ttk, Text
+from tkinter import Tk, ttk, Text, Button
 from googletrans import Translator
 
 translator = Translator()
 
-def traduzir():
-    text = ''
-    src = ''
-    dest = ''
-    result = translator.translate()
-    return result
+def translate():
+    text = input_text.get('1.0', 'end')
+    input_language = combo_input.get()
+    output_language = combo_output.get()
+    result = translator.translate(text=text, src=input_language, dest=output_language)
+    output_text.configure(state='normal')
+    output_text.delete('1.0', 'end')
+    output_text.insert('1.0',result.text)
+    output_text.configure(state='disabled')
+    # return result
 
 window = Tk()
 window.title('Translator')
@@ -27,7 +31,8 @@ label_input = ttk.Label(
 combo_input = ttk.Combobox(
     frame_input,
     values=language_list,
-    font=(None, 20)
+    font=(None, 20),
+    state='readonly'
 )
 combo_input.set('pt')
 
@@ -51,17 +56,22 @@ label_output = ttk.Label(
 combo_output = ttk.Combobox(
     frame_output,
     values=language_list,
-    font=(None, 20)
+    font=(None, 20),
+    state='readonly'
 )
 combo_output.set('en')
 label_output.grid(row=0, column=0, padx=10, pady=10)
 combo_output.grid(row=0, column=1)
 frame_output.pack()
 
-output_text = Text()
+output_text = Text(state='disabled')
 output_text.pack(padx=10, pady=20, fill='both', expand='yes')
 
-button = ttk.Button(text='Translate!')
-button.pack()
+button = Button(
+    text='Translate!',
+    font=(None, 20),
+    command=translate
+)
+button.pack(fill='both', padx=10, pady=10)
 
 window.mainloop()
