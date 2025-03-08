9deb76467e56ebab9b2dcb9ad2d27abb9907c52f
otonielnn <otoniel_2002@live.com>
2025-03-08 13:48:34 -0300

    Feat: editar tradução no histórico
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- translator.py
+++ translator.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, Text
+from tkinter import ttk, Text, Toplevel
 from googletrans import Translator
 from ttkbootstrap import Style
 
@@ -24,17 +24,60 @@ def translate(event=None):
     with open('traducoes.txt', mode='a', newline='', encoding='UTF-8') as arquivo:
         arquivo.write(f'{text.strip()}|{input_language}|{result.text.strip()}|{output_language}\n')
 
-def delete_translation(tabela):
-    linha_selecionada = tabela.selection()
-    if linha_selecionada:
-        tabela.delete(linha_selecionada)
+def delete_translation(table):
+    selected_line = table.selection()
+    if selected_line:
+        table.delete(selected_line)
         with open('traducoes.txt', mode='w', newline='', encoding='UTF-8') as arquivo:
-            for linha in tabela.get_children():
-                linha = tabela.item(linha)['values']
+            for linha in table.get_children():
+                linha = table.item(linha)['values']
                 arquivo.write(f'{linha[0]}|{linha[1]}|{linha[2]}|{linha[3]}\n')
 
+def edit_translation(table, window):
+
+    edit_translation_frame = ttk.Frame(window)
+    edit_translation_frame.pack(fill='both', expand='yes')
+
+    label_translation = ttk.Label(
+    edit_translation_frame,
+    text='Edit Tranlation:',
+    font=(None, 20)
+)
+    label_translation.pack(padx=10, fill='both', expand='yes')
+
+    new_translation = Text(edit_translation_frame, height=10, width=50)
+    new_translation.pack(padx=10, fill='both', expand='yes')
+
+    selected_line = table.selection()
+
+    if not selected_line:
+        return
+    
+    if selected_line:
+        line = table.item(selected_line)['values']
+        translation = line[2]
+        new_translation.insert('1.0', translation)
+    
+    def save_translation():
+        new_text = new_translation.get('1.0', 'end').strip()
+        table.item(selected_line, values=(line[0], line[1], new_text, line[3]))
+        with open('traducoes.txt', mode='w', newline='', encoding='UTF-8') as arquivo:
+            for linha in table.get_children():
+                linha = table.item(linha)['values']
+                arquivo.write(f'{linha[0]}|{linha[1]}|{linha[2]}|{linha[3]}\n')
+        window.destroy()
+
+    button_save_translation = ttk.Button(edit_translation_frame, text='Save Translation', command=save_translation)
+    button_save_translation.pack(fill='both', padx=10, pady=10)
+    
+    button_cancel = ttk.Button(edit_translation_frame, text='Cancel', command=edit_translation_frame.destroy)
+    button_cancel.pack(fill='both', padx=10, pady=10)
+
 def history():
-    history_frame = ttk.Frame(global_frame)
+    history_window = Toplevel(window)
+    history_window.title('History')
+
+    history_frame = ttk.Frame(history_window)
     history_frame.pack(fill='both', expand='yes')
 
     tabela = ttk.Treeview(history_frame, columns=('Input', 'Input Language', 'Output', 'Output Language'), show='headings')
@@ -55,10 +98,14 @@ def history():
 
     tabela.pack(fill='both', expand='yes', padx=10, pady=10)
 
-    button_close_history = ttk.Button(history_frame, text='Close History', command=lambda: history_frame.destroy())
+    button_close_history = ttk.Button(history_frame, text='Close History', command=lambda: history_window.destroy())
     button_close_history.pack(fill='both', padx=10, pady=10)
+
     button_delete_translation = ttk.Button(history_frame, text='Delete Translation', command=lambda: delete_translation(tabela))
     button_delete_translation.pack(fill='both', padx=10, pady=10)
+
+    button_edit_translation = ttk.Button(history_frame, text='Edit Translation', command=lambda: edit_translation(tabela, history_window))
+    button_edit_translation.pack(fill='both', padx=10, pady=10)
 
 language_list = ['pt', 'es', 'en', 'fr', 'de', 'it', 'ru', 'ja', 'ko', 'ar', 'hi']
 
